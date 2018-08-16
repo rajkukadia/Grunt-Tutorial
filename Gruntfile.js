56be494ca915bd3be2b2f06c53fa55aaece149bf
Raj Kukadia <rkukadia@intralinks.com>
2018-08-16 14:33:57 -0400

    grunt tasks and config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,36 @@
-module.exports = function(grunt){
-  //grunt gives access to grunt api
+module.exports = function(grunt){ /* -> 'grunt' parameter gives access to grunt api
+                                    */
+
+    grunt.initConfig({ /* -> Config object to initialize variables like 'greeting'
+                          -> You can read JSON file too, like package.json
+                        */
+        greeting: 'Welcome',
+        pkg: grunt.file.readJSON('package.json'),
+        age: 'I am <%= 5 + 5 %>', /* -> Used Javascript code inside <%=  %> */
+        today: 'today is <%= grunt.template.today("yyyy-mm-dd")%>'
+    });
+
     //3 params: name, desc, to do function
     grunt.registerTask('greet', 'greet the user', function(){
-        console.log('1. grunt is running!'); //use command -> grunt greet -> it will display grunt is running
+        console.log('Grunt is running!'); /* -> command -> grunt greet -> it will display grunt is running */
+        console.log(grunt.config('greeting'));
+        console.log(grunt.config('pkg.name'));
+        console.log(grunt.config('age'));
+        console.log(grunt.config('today'));
     })
 
     grunt.registerTask('work', 'do some work', function(who, howmuch){
-        console.log('2. working!'); /* use -> grunt greet work -> to do both tasks
-                                       -> command -> grunt work:Joe:5minutes ->
-                                    */
+        console.log(who+' is working since '+howmuch); /* -> use command -> grunt greet work -> to do both tasks
+                                                          -> command -> grunt work:Joe:5minutes -> to pass parameters to the function
+                                                          -> to use spaces, write in quotes  grunt work:Joe:"5 min"
+                                                        */
     })
+
+    grunt.registerTask('default', ['greet', 'work:Joe:2 minutes']); /*  -> use 'default' task name to run grunt without passing any command line arguments
+                                                                        -> It runs all the task names mentioned in sequence in the array
+                                                                        -> command -> grunt
+                                                                    */
 };
+
+
+
